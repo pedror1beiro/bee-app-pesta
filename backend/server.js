084f28189aa43c73d397a4d0e45a8131585b41b7
unknown <p3droribeiro21@gmail.com>
2026-05-19 13:48:50 +0100

    feat: configurar produção de urls

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:5173',
+        'https://bee-app-pesta.vercel.app',
         process.env.FRONTEND_URL || '*'
     ]
 }));
